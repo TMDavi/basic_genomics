1d146664094b94c18ade6f1b5b09884196aac307
TMDavi <davidtavaresmartins2@gmail.com>
2026-04-11 14:53:13 -0400

    Added quality control and assembly steps

---
+++ 00_set_environment/download_data.sh
@@ -0,0 +1,3 @@
+#Mycbacterium genitalitum 
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/041/SRR11601741/SRR11601741_1.fastq.gz -O Reads/mycogenitalium_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR116/041/SRR11601741/SRR11601741_2.fastq.gz -O Reads/mycogenitalium_2.fastq.gz
